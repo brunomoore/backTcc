112b32492651c30bf2f9942d1e5c15d8b8973329
Bruno Moore <brunomoore22@gmail.com>
2018-11-12 19:21:26 -0200

    att

--- backendCFP/src/main/java/com/serv/entities/User.java
+++ backendCFP/src/main/java/com/serv/entities/User.java
@@ -3,14 +3,11 @@ package com.serv.entities;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.springframework.context.annotation.Scope;
